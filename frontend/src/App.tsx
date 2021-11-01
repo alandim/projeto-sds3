d1e9e1c4cb34537402d4b8ea353957ab04e52fba
Andre Landim <andrelandim_4@hotmail.com>
2021-11-01 15:11:34 -0300

    Atualização rodapé

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo</h1>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo</h1>
+      </div>
+      <Footer></Footer>
+    </>
   );
 }
 
