a35d77f4813ec3f14193ae0ea4418af1564bd16a
Andre Landim <andrelandim_4@hotmail.com>
2021-11-03 10:41:23 -0300

    Security config

---
+++ frontend/public/Untitled-1.java
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+<modificador de acesso> class NomeDaClasse {
+
+	// Local onde atributos, construtores e métodos são criados
+    
+}
+
+
+public class Texto{
+    public static void main(String[] args) {
+
+        System.out.println("André Landim Pereira");
+        System.out.print("Antônio \n Vieira \n dos\n Santo\n");
+        System.out.printf("%s\n %s\n", "Marcela", "Nogueira");
+
+    }
+}
+
+
+
+
+
